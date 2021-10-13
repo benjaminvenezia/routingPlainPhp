48cae5d3bac6a1d963ccc62829a1f1be0352543a
Venezia Benjamin <benjamin.venezia@golem.ai>
2021-10-13 16:51:52 +0200

    finish the basic route system. (without poo)

--- index.php
+++ index.php
@@ -1,74 +1,52 @@
 <?php
 
-/**
- * BIENVENUE DANS CE COURS SUR LE COMPOSANT SYMFONY/ROUTING !
- * ----------------------
- * Dans ce cours nous allons étudier ce fabuleux composant qui permet de créer et de gérer des routes (adresses) intelligentes et intelligibles !
- * 
- * PRESENTATION DE L'APPLICATION (SIMPLE) :
- * ----------------
- * Nous avons ici une application de gestion de tâches très simple : pas de base de données, pas de réelles manipulations de données, ce n'est
- * qu'à des fins d'exemples.
- * 
- * Elle possède 3 pages distinctes :
- * - /index.php (ou /index.php?page=list) : permet d'afficher la liste des tâches contenue dans le fichier data.php (voir le fichier pages/list.php)
- * - /index.php?page=show&id=100 : permet d'afficher la tâche dont l'identifiant est 100 en détails (voir le fichier pages/show.php)
- * - /index.php?page=create (en GET) : permet d'afficher le formulaire de création (voir le fichier pages/create.php)
- * - /index.php?page=create (en POST) : permet de traiter le formulaire de création (toujours dans pages/create.php)
- * 
- * CREER DES ROUTES PERSONNALISEES (ET JOLIES ?) :
- * ----------------
- * On souhaite désormais pouvoir gérer tout ça avec des routes plus "propres" :
- * - /index.php (ou /index.php?page=list) deviendrait juste / 
- * - /index.php?page=show&id=100 deviendrait /show/100
- * - /index.php?page=create (en GET) deviendrait /create (en GET)
- * - /index.php?page=create (en POST) deviendrait /create (en POST)
- * 
- * Ca vous dit ? Alors commencez par bien analayser l'application dans son état actuel pour bien la comprendre, et passez à la section suivante
- * 
- */
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
 
-/**
- * LES PAGES DISPONIBLES
- * ---------
- * Afin de pouvoir être sur que le visiteur souhaite voir une page existante, on maintient ici une liste des pages existantes
- */
-$availablePages =  [
-    'list', 'show', 'create'
-];
+require __DIR__ . '/vendor/autoload.php';
 
-// Par défaut, la page qu'on voudra voir si on ne précise pas (par exemple sur /index.php) sera "list"
-$page = 'list';
+//ROUTES
+$listRoute = new Route('/');
 
-// Si on nous envoi une page en GET, on la prend en compte (exemple : /index.php?page=create)
-if (isset($_GET['page'])) {
-    $page = $_GET['page'];
-}
+$createRoute = new Route('/create', [], [], [], 'localhost', ['http', 'https'], ['POST', 'GET']); // /index.php?page=create => /create
 
-// Si la page demandée n'existe pas (n'est pas dans le tableau $availablePages)
-// On affiche la page 404
-if (!in_array($page, $availablePages)) {
+$showRoute = new Route('/show/{id<\d+>?100}', []); // revient à écrire '/show/{id}', ['id' => 100], ['id' => '\d+']);
+
+$helloRoute = new Route(
+    '/hello/{name}', 
+    ['name' => 'World', 'toto' => 42],
+);
+
+//AJOUT A LA COLLECTION 
+$collection = new RouteCollection();
+$collection->add('list', $listRoute);
+$collection->add('create', $createRoute);
+$collection->add('show', $showRoute);
+$collection->add('hello', $helloRoute);
+
+//DECLARATION DU MATCHER ET GENERATOR
+$matcher = new UrlMatcher($collection, new RequestContext('', $_SERVER['REQUEST_METHOD']));
+$generator = new UrlGenerator($collection, new RequestContext());
+
+//PATH INFO
+$pathInfo = $_SERVER['PATH_INFO'] ?? '/';
+
+
+try {
+    $currentRoute = $matcher->match($pathInfo);
+    
+    $page = $currentRoute['_route'];
+    require_once "pages/$page.php";
+} catch(ResourceNotFoundException $e){
     require 'pages/404.php';
     return;
 }
 
-/**
- * ❌ ATTENTION DEMANDEE !
- * -----------
- * Ici, un moyen simple d'obeir au visiteur et de lui présenter ce qu'il demande c'est d'inclure le fichier qui porte le même nom que la 
- * variable $page. 
- * 
- * => EXTREMENT DANGEREUX ! Ca veut dire que le visiteur pilote l'inclusion de scripts PHP, quelqu'un de malin pourrait s'en servir pour inclure 
- * un script non prévu ou voulu. On est un peu protégé par la condition juste au dessus, mais c'est quand même HYPER LIMITE.
- * 
- * Comment allons nous réparer ça dans les prochaines sections ?
- * 
- * ❌ AUTRE PROBLEME DE TAILLE ICI : LE COUPLAGE DE L'URL ET DES NOMS DE FICHIERS
- * ------------
- * Le fichier que l'on va inclure porte le même nom que le paramètre $_GET['page']. C'est à dire que si on appelle /index.php?page=create
- * c'est le fichier pages/create.php qui va être inclus.
- * 
- * La conséquence, c'est que si demain je décide que le formulaire de création devrait se trouver sur /index.php?page=new il faudra que je
- * renomme forcément le fichier pages/create.php en pages/new.php et inversement (l'enfer)
- */
-require_once "pages/$page.php";
+
+
+
+
--- pages/show.php
+++ pages/show.php
@@ -1,21 +1,9 @@
 <?php
 
-/**
- * LA PAGE DE DETAILS D'UNE TÂCHE
- * -------------
- * Elle appelée par l'url /index.php?page=show&id=100 par exemple et elle affiche les détails de la tâche qui correspond à l'id précisé en GET
- */
-
-// On appelle la liste des tâches
 $data = require_once "data.php";
 
 // Par défaut, on imagine qu'aucun id n'a été précisé
-$id = null;
-
-// Si un id est précisé en GET, on le prend
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-}
+$id = $currentRoute['id'];
 
 // Si aucun id n'est passé ou que l'id n'existe pas dans la liste des tâches, on arrête tout !
 if (!$id || !array_key_exists($id, $data)) {
@@ -42,7 +30,7 @@ $task = $data[$id];
     <p>
         La tâche est <strong><?= $task['completed'] ? "complétée" : "encore à faire" ?> !</strong>
     </p>
-    <a href="index.php">Retour à la liste</a> ou <a href="index.php?page=create">Créer une autre tâche</a>
+    <a href="<?= $generator->generate('list') ?>">Retour à la liste</a> ou <a href="<?= $generator->generate('create') ?>">Créer une autre tâche</a>
 </body>
 
 </html>
